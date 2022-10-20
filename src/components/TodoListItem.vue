65aae429381aacb67b4b40223ae9a27a4bd647a8
lukas-xrz <lukasoliveiracks@gmail.com>
2022-10-20 09:54:42 -0300

    x

--- src/components/TodoListItem.vue
+++ src/components/TodoListItem.vue
@@ -145,6 +145,38 @@ export default {
       type: Object,
       required: true,
     },
+<<<<<<< HEAD
+=======
+    batata: null
+  },
+  methods: {
+      handleEditClick(v) {
+      this.teste = v.id
+      this.$emit("edit", v);
+    }
+  },
+    watch: {
+      batata(){
+        debugger
+      }
+  },
+
+  setup(props, { emit }) {
+    const formattedTimestamp = computed(() => {
+      const date = new Date(props.item.timestamp);
+      
+      return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+      components: {CheckboxEvent}
+    });
+
+    function handleDeleteClick() {
+      emit("delete", props.item);
+    }
+    return {
+      formattedTimestamp,
+      handleDeleteClick,
+    };
+>>>>>>> fa91cad (x)
   },
 };
 </script>
